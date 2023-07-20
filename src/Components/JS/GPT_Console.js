3859d534eac592f5956831a95f7b3ae07ea33f31
danielgiron <73446404+danielgiron@users.noreply.github.com>
2023-07-19 21:55:07 -0400

    connected front end to backend

--- src/Components/JS/GPT_Console.js
+++ src/Components/JS/GPT_Console.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "../CSS/GPT_Console.css";
+import axios from "axios";
 
 function GPT_Console(props) {
+  const endpoint = "http://localhost:3001/gpt";
+  const body = { message: null };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  async function startStory() {
+    await axios
+      .post(endpoint, body)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <div className="GPT_Console">
-      <div>GPT_Console</div>
+      {/* <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div className="inputField">
+          <label htmlFor="">Name</label>
+          <input type="text" id="name_signUp" name="name" required />
+        </div>
+        <button>Submit</button>
+      </form> */}
+      <button
+        onClick={() => {
+          startStory();
+        }}
+      >
+        GPT!
+      </button>
     </div>
   );
 }
