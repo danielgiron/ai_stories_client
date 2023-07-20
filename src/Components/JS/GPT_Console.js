5d5fa9965254e41a6bd72cfbc99d0ea6dd9ad7a4
danielgiron <73446404+danielgiron@users.noreply.github.com>
2023-07-19 21:26:42 -0400

    first commit

---
+++ src/Components/JS/GPT_Console.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../CSS/GPT_Console.css";
+
+function GPT_Console(props) {
+  return (
+    <div className="GPT_Console">
+      <div>GPT_Console</div>
+    </div>
+  );
+}
+
+export default GPT_Console;
