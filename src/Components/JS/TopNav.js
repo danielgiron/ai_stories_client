33b7ac21e169cb4214b00f5449fdcc0d2c693cc6
danielgiron <73446404+danielgiron@users.noreply.github.com>
2023-09-15 00:02:16 -0400

    Restructured TopNav to match App grid layout

--- src/Components/JS/TopNav.js
+++ src/Components/JS/TopNav.js
@@ -3,16 +3,18 @@ import "../CSS/TopNav.css";
 
 function TopNav(props) {
   return (
-    <div id="TopNav">
-      <div id="Logo">
-        a<span>Irina</span>
+    <nav id="TopNav">
+      <div id="TopNavContainer">
+        <div id="Logo">
+          a<span>Irina</span>
+        </div>
+        <div id="Menu">
+          <div className="Menu-Line Top" />
+          <div className="Menu-Line Center" />
+          <div className="Menu-Line Bottom" />
+        </div>
       </div>
-      <div id="Menu">
-        <div className="Menu-Line Top" />
-        <div className="Menu-Line Center" />
-        <div className="Menu-Line Bottom" />
-      </div>
-    </div>
+    </nav>
   );
 }
 
