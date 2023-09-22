0cbe157ca6760efa7e3a435e6a91875a972d729b
danielgiron <73446404+danielgiron@users.noreply.github.com>
2023-09-21 21:27:19 -0400

    Home page blocked out

--- src/Components/JS/AnimatedRoutes.js
+++ src/Components/JS/AnimatedRoutes.js
@@ -11,11 +11,60 @@ function AnimatedRoutes(props) {
           element={
             <div className="BodyContainer">
               <HeroSection />
-              <div className="AssessmentsContainer">
-                <div className="Assessments">Assessments</div>
+              <div className="AssessmentsContainer GridContainer">
+                <div className="Assessments">
+                  <div className="SectionText">
+                    <h3>
+                      The <span>Assessments</span>
+                    </h3>
+                    <p>
+                      I'm a Developer who loves exploring new technologies to
+                      bring creative solutions to life. Check out the
+                      technologies I use below to see how I can help bring your
+                      ideas to life.
+                    </p>
+                  </div>
+                  <div className="Carousel">
+                    <div className="Card">
+                      <h4>The First One</h4>
+                    </div>
+                    <div className="Card">
+                      <h4>The Second One</h4>
+                    </div>
+                    <div className="Card">
+                      <h4>The Third One</h4>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="ResultsContainer">
-                <div className="Results">Results</div>
+              <div className="ResultsContainer GridContainer">
+                <div className="Results">
+                  <div className="SectionText">
+                    <h3>
+                      Your <span>Results</span>
+                    </h3>
+                    <p>
+                      I'm a Developer who loves exploring new technologies to
+                      bring creative solutions to life. Check out the
+                      technologies I use below to see how I can help bring your
+                      ideas to life.
+                    </p>
+                  </div>
+                  <div className="ResultEntryContainer">
+                    <div className="ResultEntry">
+                      <div className="ResultEntryName">First Result</div>
+                      <button>More</button>
+                    </div>
+                    <div className="ResultEntry">
+                      <div className="ResultEntryName">Second Result</div>
+                      <button>More</button>
+                    </div>
+                    <div className="ResultEntry">
+                      <div className="ResultEntryName">Third Result</div>
+                      <button>More</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           }
--- src/Components/JS/HeroSection.js
+++ src/Components/JS/HeroSection.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Cat from "../../Media/Images/Cat.JPG";
 import "../CSS/Hero.css";
 
 function HeroSection(props) {
   return (
     <section className="HeroContainer">
-      <div id="Hero">Hero Section</div>
+      <div id="Hero">
+        <div className="HeroText">
+          <h1>
+            Mirrors of <span>Identity</span>
+          </h1>
+          <p>
+            This site is a tribute to Irina and artificial intelligence, two
+            mirrors that uniquely reflect facets of my own identity. Through
+            their unpredictable behaviors and complex logical processes, I find
+            a deeper understanding of myself.
+          </p>
+          <button>See For Yourself</button>
+        </div>
+        <div className="HeroVisual">
+          <img src={Cat} />
+        </div>
+      </div>
     </section>
   );
 }
