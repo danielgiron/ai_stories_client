a87efcfc6b98d8dfd567d973d6dc5a1405527c3c
danielgiron <73446404+danielgiron@users.noreply.github.com>
2024-03-26 14:57:09 -0400

    New CSS class GridLayout applied to App Footer

--- src/Components/JS/Footer.js
+++ src/Components/JS/Footer.js
@@ -3,8 +3,10 @@ import "../CSS/Footer.css";
 
 function Footer(props) {
   return (
-    <div className="FooterContainer">
-      <footer id="Footer">Footer</footer>
+    <div className="FooterContainer ">
+      <footer id="Footer" className="GridLayout">
+        Footer
+      </footer>
     </div>
   );
 }
